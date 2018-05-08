9a462c00d2cd022ec7270b6dfb1b8f2601f965e4
Jonathan Caiza <jpaul.snp@gmail.com>
2018-05-08 15:49:51 -0500

    Presentacion de datos
    Edicion de app.component.css, app.component.html y app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre = 'Jonathan';
+  apellido =  'Caiza';
+  edad = 23;
+  sueldo = 1555.00;
+  fechaMacimiento = new Date('1995-03-29');
+  nombreBoton = 'cambiar color';
+  nombreClase = 'sa-titulo';
+  cambiarClaseDeTitulo() {
+    const nombreClaseEsTitulo = this.nombreClase === 'sa-titulo';
+    if (nombreClaseEsTitulo) {
+      this.nombreClase = 'sa-subtitulo';
+    } else {
+      this.nombreClase = 'sa-titulo';
+    }
+  }
+
+  aplicarClase(valorImput: string) {
+    this.nombreClase = valorImput;
+  }
 }
