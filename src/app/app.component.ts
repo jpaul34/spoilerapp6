65d17de1920fd4afbfb88f431bb9a07e410f0b1f
jpaul34 <jpaul.snp@gmail.com>
2018-05-11 18:25:41 -0500

    Creacion del Componente de Pelicula
    Aplicacion SpoilerApp con el componente Pelicula

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,83 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre = 'Jonathan';
-  apellido =  'Caiza';
-  edad = 23;
-  sueldo = 1555.00;
-  fechaMacimiento = new Date('1995-03-29');
-  nombreBoton = 'cambiar color';
-  nombreClase = 'sa-titulo';
-  cambiarClaseDeTitulo() {
-    const nombreClaseEsTitulo = this.nombreClase === 'sa-titulo';
-    if (nombreClaseEsTitulo) {
-      this.nombreClase = 'sa-subtitulo';
-    } else {
-      this.nombreClase = 'sa-titulo';
-    }
-  }
 
-  aplicarClase(valorImput: string) {
-    this.nombreClase = valorImput;
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // nombre = 'Jonathan';
+  // apellido =  'Caiza';
+  // edad = 23;
+  // sueldo = 1555.00;
+  // fechaMacimiento = new Date('1995-03-29');
+  // nombreBoton = 'cambiar color';
+  // nombreClase = 'sa-titulo';
+  // clase = 'papa'
+  // cambiarClaseDeTitulo() {
+  //   const nombreClaseEsTitulo = this.nombreClase === 'sa-titulo';
+  //   if (nombreClaseEsTitulo) {
+  //     this.nombreClase = 'sa-subtitulo';
+  //   } else {
+  //     this.nombreClase = 'sa-titulo';
+  //   }
+  // }
+  //
+  // aplicarClase(valorImput: string) {
+  //   this.nombreClase = valorImput;
+  // }
 }
---
+++ src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pelicula-banner',
+  templateUrl: './pelicula-banner.component.html',
+  styleUrls: ['./pelicula-banner.component.css']
+})
+export class PeliculaBannerComponent implements OnInit {
+  urlImagen = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL_FOh_xn19xgXf_Uq5OK3WWzAmoVoZLHCO2vjFd4LKaxijU59eg';
+  descripcionImagen = 'Imagen de la pelicula Tomb Raider';
+  nombrePelicula = 'Tomb Raider';
+  descripcionPelicula = 'Alicia vikander,Walton Googins';
+  esEstreno = false;
+  textoEstreno: string;
+  claseEstreno: string;
+  constructor() { }
+
+
+  ngOnInit() {
+    if (this.esEstreno) {
+      this.textoEstreno = 'Estreno';
+      this.claseEstreno = 'sa-color-estado-rosado';
+    } else {
+      this.textoEstreno = 'Proximamente';
+      this.claseEstreno = 'sa-color-estado-amarillo';
+    }
+  }
+}
